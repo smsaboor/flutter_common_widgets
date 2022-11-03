ce2a63783dfe41affb82ecfed53ab2184468a5c5
saboorows <mca.saboor@gmail.com>
2022-11-03 15:16:45 +0530

    finished first version

---
+++ lib/flutter_package1.dart
@@ -0,0 +1,27 @@
+library flutter_package1;
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  var onPressed;
+  final Widget? child;
+  var style;
+  CustomButton({Key? key, @required this.onPressed, this.child, this.style})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+        onPressed: onPressed,
+        style: TextButton.styleFrom(
+          padding: const EdgeInsets.all(16.0),
+          primary: Colors.white,
+          backgroundColor: Colors.blue,
+          elevation: 9.0,
+          textStyle: const TextStyle(
+            fontSize: 20,
+          ),
+        ),
+        child: child!
+    );
+  }
+}
