e747a992dc7fca0ee68d36a611524591a51f2ce4
saboorows <mca.saboor@gmail.com>
2022-11-03 15:58:29 +0530

    finished first version-2

---
+++ lib/navigation.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void navigateTo(context, widget) => Navigator.push(
+  context,
+  MaterialPageRoute(
+    builder: (context) => widget,
+  ),
+);
+void navigateAndFinsh(context, widget) => Navigator.pushAndRemoveUntil(
+  context,
+  MaterialPageRoute(
+    builder: (context) => widget,
+  ),
+      (route) => false,
+);
