5d0d5f94c2d849a431e5d37867957b4d824f05d5
smsaboor <mca.saboor@gmail.com>
2022-11-15 12:34:55 +0530

    finished first version20

--- lib/package2/date.dart
+++ lib/package2/date.dart
@@ -4,11 +4,16 @@ import 'package:intl/intl.dart';
 
 class CustomDate extends StatefulWidget {
   const CustomDate(
-      {Key? key, required this.controller, required this.validator,required this.labelText})
+      {Key? key,
+      required this.controller,
+      required this.validator,
+      required this.labelText,
+      required this.year})
       : super(key: key);
   final controller;
   final validator;
   final labelText;
+  final year;
 
   @override
   State<CustomDate> createState() => _CustomDateState();
@@ -17,48 +22,36 @@ class CustomDate extends StatefulWidget {
 class _CustomDateState extends State<CustomDate> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(15.0),
-      padding: const EdgeInsets.only(left: 5.0),
-      height: 80,
-      width: MediaQuery.of(context).size.width,
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            child: DateTimeField(
-              controller: widget.controller,
-              //editable: false,
-              validator: widget.validator,
-              decoration: InputDecoration(
-                focusedBorder: const OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.red, width: 1.0),
-                ),
-                enabledBorder: const OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.black26, width: 1.0),
-                ),
-                border: const OutlineInputBorder(),
-                labelText: widget.labelText,
-                labelStyle: const TextStyle(
-                  fontSize: 14.0,
-                ),
-              ),
-              format: DateFormat("yyyy-MM-dd"),
-              onShowPicker: (context, currentValue) {
-                return showDatePicker(
-                  context: context,
-                  initialDate: DateTime.now(),
-                  firstDate: DateTime.now().subtract(const Duration(days: 0)),
-                  lastDate: DateTime.now().add(const Duration(days: 240)),
-                );
-              },
-              onChanged: (dt) {
-                // _endDateController.text =
-                //     new DateFormat("yyyy-MM-dd").format(dt?.add(new Duration(days: 354)) ?? DateTime.now());
-              },
-            ),
-          ),
-        ],
+    return DateTimeField(
+      controller: widget.controller,
+      //editable: false,
+      validator: widget.validator,
+      decoration: InputDecoration(
+        focusedBorder: const OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.red, width: 1.0),
+        ),
+        enabledBorder: const OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.black26, width: 1.0),
+        ),
+        border: const OutlineInputBorder(),
+        labelText: widget.labelText,
+        labelStyle: const TextStyle(
+          fontSize: 14.0,
+        ),
       ),
+      format: DateFormat("yyyy-MM-dd"),
+      onShowPicker: (context, currentValue) {
+        return showDatePicker(
+          context: context,
+          initialDate: DateTime.utc(widget.year),
+          firstDate: DateTime.now().subtract(const Duration(days: 0)),
+          lastDate: DateTime.now().add(const Duration(days: 240)),
+        );
+      },
+      onChanged: (dt) {
+        // _endDateController.text =
+        //     new DateFormat("yyyy-MM-dd").format(dt?.add(new Duration(days: 354)) ?? DateTime.now());
+      },
     );
   }
 }
